9d5b40dea05ba1882a27642c59ab28b4d87cb32d
Daryn Brown <darynnbrown@gmail.com>
2022-03-21 16:51:04 -0500

    added and completed blog cards

--- src/App.vue
+++ src/App.vue
@@ -69,4 +69,29 @@ export default {
     fill: #fff; 
   }
 }
+
+.blog-card-wrap {
+  position: relative;
+  padding: 80px 16px;
+  background-color: #f1f1f1;
+  @media(min-width: 500px) {
+    padding: 100px 16px;
+  }
+
+  .blog-cards {
+    display: grid;
+    gap: 32px;
+    grid-template-columns: 1fr; 
+
+    @media(min-width: 500px) {
+      grid-template-columns: repeat(2, 1fr); 
+    }
+    @media(min-width: 900px) {
+      grid-template-columns: repeat(3, 1fr); 
+    }
+    @media(min-width: 1200px) {
+      grid-template-columns: repeat(4, 1fr); 
+    }
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,23 @@
   <div class="home">
     <BlogPost :post="welcomeScreen" />
     <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCards :post="post" v-for="(post, index) in sampleBlogCards"  :key="index" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "../components/BlogPost"
+import BlogCards from "../components/BlogCard"
 export default {
   name: "Home",
-  components: {BlogPost},
+  components: {BlogPost, BlogCards},
   data() {
     return {
       welcomeScreen: {
@@ -31,7 +40,25 @@ export default {
             blogCoverPhoto: "designed-for-everyone",
         },
       ],
+      sampleBlogCards: [
+        { blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "May 1, 2021" },
+        { blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "May 1, 2021" },
+        { blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "May 1, 2021" },
+        { blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "May 1, 2021" },
+      ],
     }; 
   },
 };
 </script>
+
+<style lang="scss" scoped>
+
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+
+</style>
