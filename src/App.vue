f425b293245be14915365beb825271f01f6abd0e
Daryn Brown <darynnbrown@gmail.com>
2022-03-30 19:56:09 -0500

    Added routing. Completed Login page.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="app-wrapper">
     <div class="app">
-      <Navigation/>
+      <Navigation v-if="!navigation" />
       <router-view />
-      <Footer/>
+      <Footer v-if="!navigation"/>
     </div>
   </div>
 </template>
@@ -15,12 +15,28 @@ export default {
   name: "app",
   components: { Navigation, Footer },
   data() {
-    return {};
+    return {
+      navigation: null,
+    };
   },
-  created() {},
+  created() {
+    this.checkRoute();
+  },
   mounted() {},
-  methods: {},
-  watch: {},
+  methods: {
+    checkRoute() {
+      if (this.$route.name === "Login" || this.$route.name === "Register" || this.$route.name === "ForgotPassword") {
+        this.navigation = true;
+        return;
+      } 
+        this.navigation = false;   
+    },
+  },
+  watch: {
+    $route() {
+      this.checkRoute(); 
+    }
+  },
 };
 </script>
 
@@ -126,7 +142,7 @@ button,
   padding: 80px 16px;
   background-color: #f1f1f1;
   @media(min-width: 500px) {
-    padding: 100px 16px;
+    padding: 270px 16px;
   }
 
   .blog-cards {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <div class="container">
         <h3>View More Recent Blogs</h3>
         <div class="blog-cards">
-          <BlogCards :post="post" v-for="(post, index) in sampleBlogCards"  :key="index" />
+          <BlogCard :post="post" v-for="(post, index) in sampleBlogCards"  :key="index" />
         </div>
       </div>
     </div>
@@ -23,11 +23,11 @@
 
 <script>
 import BlogPost from "../components/BlogPost"
-import BlogCards from "../components/BlogCard"
+import BlogCard from "../components/BlogCard"
 import Arrow from "../assets/Icons/arrow-right-light.svg"
 export default {
   name: "Home",
-  components: {BlogPost, BlogCards, Arrow},
+  components: {BlogPost, BlogCard, Arrow},
   data() {
     return {
       welcomeScreen: {
@@ -49,13 +49,12 @@ export default {
             blogCoverPhoto: "designed-for-everyone",
         },
       ],
-      sampleBlogCards: [
-        { blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "May 1, 2021" },
-        { blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "May 1, 2021" },
-        { blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "May 1, 2021" },
-        { blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "May 1, 2021" },
-      ],
     }; 
+  },
+  computed: {
+    sampleBlogCards() {
+      return this.$store.state.sampleBlogCards; 
+    },
   },
 };
 </script>
